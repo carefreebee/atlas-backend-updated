c2c60367cbeeee90c8df56f2253530bac1ac6e8b
Chae-vin <arvin.santillan@cit.edu>
2024-09-23 20:51:39 +0800

    removed imports

--- src/main/java/com/example/Atlas/Entity/InternalEntity.java
+++ src/main/java/com/example/Atlas/Entity/InternalEntity.java
@@ -1,7 +1,5 @@
 package com.example.Atlas.Entity;
 
-import java.time.LocalDate;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/example/Atlas/Repository/primaryInternalRepository.java
+++ src/main/java/com/example/Atlas/Repository/primaryInternalRepository.java
@@ -1,7 +1,6 @@
 package com.example.Atlas.Repository;
 
 import com.example.Atlas.Entity.primaryInternalEntity;
-import com.example.Atlas.Entity.primaryStakeholderEntity;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
